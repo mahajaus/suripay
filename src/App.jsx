90321418ba6ffedf87abb325af6e90f691de0264
mahajaus <134316264+mahajaus@users.noreply.github.com>
2026-05-23 15:36:29 -0300

    Add WhatsApp input field to KYC

--- src/App.jsx
+++ src/App.jsx
@@ -785,7 +785,7 @@ function KYCScreen({ user }) {
     <span style={{ fontSize: 20 }}>📱</span>
     <input
       type="tel"
-      placeholder="+597XXXXXXX"
+      placeholder="+597 XXX XXXX"
       value={whatsapp}
       onChange={e => setWhatsapp(e.target.value)}
       style={{
